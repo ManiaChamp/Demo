0fd20af1ac2a3f3b0cc6495d4f5c8db46707ee37
Manish <Manish Sharma>
2017-09-19 14:04:28 +0530

    Myframework

---
+++ MyFrameWork/MyFrameWork/MyFrameWork.h
@@ -0,0 +1,19 @@
+//
+//  MyFrameWork.h
+//  MyFrameWork
+//
+//  Created by Manish on 19/09/17.
+//  Copyright © 2017 Manish Sharma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyFrameWork.
+FOUNDATION_EXPORT double MyFrameWorkVersionNumber;
+
+//! Project version string for MyFrameWork.
+FOUNDATION_EXPORT const unsigned char MyFrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyFrameWork/PublicHeader.h>
+
+
---
+++ MyFrameWork/MyFrameWork/test.swift
@@ -0,0 +1,14 @@
+//
+//  test.swift
+//  MyFrameWork
+//
+//  Created by Manish on 19/09/17.
+//  Copyright © 2017 Manish Sharma. All rights reserved.
+//
+
+import Foundation
+
+class  test: NSObject {
+    
+    
+}
